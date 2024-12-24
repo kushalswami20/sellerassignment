7535e1def50ed1407f4d47796a4b32918cb38787
kushalswami20 <161000509+kushalswami20@users.noreply.github.com>
2024-12-24 14:20:41 +0530

    Update signup.jsx

--- hosteecommerce/src/components/admin/signup.jsx
+++ hosteecommerce/src/components/admin/signup.jsx
@@ -46,7 +46,7 @@ const AdminSignup = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5001/admin/seller/signup', {
+      const response = await fetch('https://sellerassignment.vercel.app/admin/seller/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
