6d1c8c65e3723046ef168530a5ff7ef6425b1423
kushalswami20 <161000509+kushalswami20@users.noreply.github.com>
2024-12-25 02:24:40 +0530

    Update app.js

--- ecommercebackend/app.js
+++ ecommercebackend/app.js
@@ -19,21 +19,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://sellerassignment.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    return res.status(200).json({
-      body: "OK"
-    });
-  }
-  
-  next();
-});
+app.use(cors());
 
 
 app.use(express.json());
